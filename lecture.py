ecf5d97b96f71631e11128ea3257b60514d4de74
PaulAdrPENET <pauladrienpenet@gmail.com>
2023-05-05 09:59:18 +0200

    lecture csv

--- lecture.py
+++ lecture.py
@@ -9,3 +9,12 @@ print(seisme)
 # 2
 nb_seisme = len(seisme)
 print('Nombre seisme = ', nb_seisme)
+
+# 3
+noms = pd.DataFrame()
+
+for pays in seisme["pays"]:
+    noms.set_index = pays
+    noms['pays'] = seisme.count(pays)
+
+print(noms.index)
