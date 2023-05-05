6fb0388d4e214d2547031a07b9e08cf2e43137ad
clement <clement.jaminion@gmail.com>
2023-05-05 13:48:11 +0200

    4

--- lecture.py
+++ lecture.py
@@ -1,4 +1,5 @@
 import matplotlib
+import matplotlib.pyplot as plt
 import seaborn
 import pandas as pd
 
@@ -13,3 +14,26 @@ print('Nombre seisme = ', nb_seisme)
 # 3
 noms = seisme.value_counts('pays', ascending=False).head(20)
 print(noms)
+
+# 4
+seisme_data = seisme[seisme['pays'].isin(noms.index)]
+print(seisme_data)
+seaborn.boxplot(data=seisme_data, x="pays", y='mag', orient="v", dodge=False)
+
+plt.xticks(rotation=90)
+plt.show()
+
+# 4b
+seisme_mag = seisme.value_counts('mag', ascending=False).tail(6)
+seisme_mag_data = seisme[seisme['mag'].isin(seisme_mag.index)]
+print("6 lieux du monde qui enregistre la plus forte magnitude: ")
+print(seisme_mag_data[['pays', 'mag']])
+
+# 4c
+seisme_californie_alaska = list()
+seisme_californie_alaska.append(seisme[(seisme['pays'] == 'Alaska') & (seisme["mag"] <= 2)])
+seisme_californie_alaska.append(seisme[(seisme['pays'] == 'California') & (seisme["mag"] <= 2)])
+print(seisme_californie_alaska)
+
+
+
